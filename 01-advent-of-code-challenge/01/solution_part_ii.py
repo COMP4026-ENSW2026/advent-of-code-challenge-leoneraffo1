956278bc8dfebcff4b41fcccfc15163c174931ae
leoneraffo1 <69177430+leoneraffo1@users.noreply.github.com>
2023-02-14 20:31:15 -0300

    Update solution_part_ii.py

---
+++ 01-advent-of-code-challenge/01/solution_part_ii.py
@@ -0,0 +1,14 @@
+calorias = 0
+elfos = []
+with open("input.txt", encoding="utf-8",mode="r")as tabela:
+  line = [(a.replace('\n', " ")) if a=='\n' else int(a.replace('\n', "")) for a in tabela.readlines()]
+  for x in line:
+    if x == " ":
+      elfos.append(calorias)
+      calorias = 0
+    else:
+      calorias += int(x)
+  print(max(elfos))
+  elfos.sort()
+  elfos.reverse()
+  print(sum(elfos[0:3]))
